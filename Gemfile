05d4a1ba3bd5cf1daef5d869843351045dd5c5f4
Eric <spb828@naver.com>
2016-05-10 05:59:52 +0900

    20160510 05:59 ERIC several changes

--- Gemfile
+++ Gemfile
@@ -1,6 +1,11 @@
 source 'https://rubygems.org'
 
-
+gem 'fuzzy-string-match'
+gem 'awesome_print'
+gem 'seed_dump'
+gem 'nokogiri'
+gem 'tzinfo-data'
+gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
 # Use sqlite3 as the database for Active Record
@@ -44,4 +49,3 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- db/migrate/20160508142700_create_songs.rb
+++ db/migrate/20160508142700_create_songs.rb
@@ -1,10 +1,10 @@
 class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
-      
+
       # 음원은 싱글이던 아니던 앨범에 속한다
       t.integer :album_id       # =>앨범아이디(1:N 릴레이션)
-      
+
       # 음원 정보(보통)
       t.string  :title          # =>제목
       t.string  :ganre1         # =>장르1
@@ -15,24 +15,24 @@ class CreateSongs < ActiveRecord::Migration
       t.string  :composer       # =>작곡
       # t.string  :artist_name    # =>아티스트 이름
       t.integer :singer_id      # =>가수아이디(1:N 릴레이션)
-      
+
       # 음원 정보(참조)
-      # t.integer :artist_num     # =>아티스트 번호(Default : 팀 단위)
+      t.integer :artist_num     # =>아티스트 번호(Default : 팀 단위)
       t.integer :team_id        # =>팀아이디(1:M 릴레이션)
       t.integer :album_num      # =>앨범 번호
-      
+
       # 음원 정보(참조추출)
       t.text    :artist_photo   # =>아티스트 사진(아티스트테이블로부터 불러와 저장)
       t.text    :jacket         # =>자켓사진(앨범테이블로부터 불러와 저장)
-      
+
       # 음원 정보(고유값)
       t.integer :song_tjnum     # =>TJ미디어 기준, 노래방번호
       t.integer :song_num       # =>음원의 지니뮤직 고유번호
-      
+
       # 음원 정보(음역)
       t.string  :lowkey         # =>음원의 가장 낮은 키
       t.string  :highkey        # =>음원의 가장 높은 키
-      
+
       t.timestamps null: false
     end
   end
