a9abce0cd1f1fd6b50eb96f4490b737a51a12a63
SOHN DAI GEUN <nhos126@gmail.com>
2016-05-27 00:45:43 +0900

    Add Google Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ActiveRecord::Base
     # :confirmable, :lockable, :timeoutable and :omniauthable
     devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable,
-    :omniauthable, :omniauth_providers => [:facebook]
+    :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
+
+
 
     has_many :mylists
 
@@ -20,6 +22,26 @@ class User < ActiveRecord::Base
         super.tap do |user|
             if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
                 user.email = data["email"] # if user.email.blank?
+            end
+        end
+    end
+
+    def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+        data = access_token.info
+        user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
+        if user
+            return user
+        else
+            registered_user = User.where(:email => access_token.info.email).first
+            if registered_user
+                return registered_user
+            else
+                user = User.create(name: data["name"],
+                provider:access_token.provider,
+                email: data["email"],
+                uid: access_token.uid ,
+                password: Devise.friendly_token[0,20],
+                )
             end
         end
     end
