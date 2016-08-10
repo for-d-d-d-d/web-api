4cf38e0b1ce4ed0a47d892ce913eca7d2d28a4c9
yhk1038 <yhk1038@hanmail.net>
2016-08-11 01:27:37 +0900

    추천 컨트롤러 1차 (#11)
    * 추천 전용 컨트롤러
    
    * 추천 컨트롤러 1차 정리 끝

--- app/controllers/json_controller.rb
+++ app/controllers/json_controller.rb
@@ -382,66 +382,14 @@ class JsonController < ApplicationController
     render json: result
   end
   
+  # Recommender
+  # method : POST, GET
+  # Input   > id: 회원 id
+  # Output  > 추천 Song Data
   def recom
-    #
-    # => Initailizer SET
-    ##########################
-    
-    users = []
-    me = []
-    it_looks_like_your_favorite_song = []
-    low_limit = 4   #    N 개
-    high_limit = 10 #    N 개
-    favor_rate = 50 #    N %
-    
-    
-    #
-    # => TEMP USER SET
-    ##########################
-    
-    user1 = ('1'..'20').to_a
-    user2 = ['1','2','3','4','5','6','7','8','9','10']
-    user3 = ['1',    '3',    '5',    '7',    '9']
-    user4 = ['1','2','3','4','5','6','95']
-    user5 = [                    '6','7','8','9','10']
-    user6 = [    '2',    '4',    '6',    '8',    '10']
-    user7 = ['1','2','3',    '5','6','7','8',        '11','12','90','91','92','93','94']
-    users = [user1, user2, user3, user4, user5, user6, user7]
-    
-    
-    me    = ['1','2','3',        '6',    '8',        '11','12']
-    
-    #
-    # => RECOMMENDATION !!!
-    ##########################
-    
-    users.each do |someone|
-      state = false
-      state_limit = false
-      state_favor = false
-      potential_recom = someone - me
-      how_many_equal = (someone - (someone - me)).count
-      
-      if how_many_equal >= low_limit
-        state_limit = true
-      end
-      # puts "#{how_many_equal}, #{someone.count}, #{how_many_equal.to_f/someone.count.to_f}"
-      if (how_many_equal.to_f/someone.count.to_f) * 100 >= favor_rate
-        state_favor = true
-      end
-      #puts state_favor
-      
-      if state_favor == true && state_limit == true
-        potential_recom.each do |song|
-          it_looks_like_your_favorite_song << song
-          break if it_looks_like_your_favorite_song.uniq.count == high_limit
-        end
-      end
-    end
-    it_looks_like_your_favorite_song.uniq!
-    
-    print "#{it_looks_like_your_favorite_song}\n"
-    render json: it_looks_like_your_favorite_song
+    recomC = RecommendationController.new
+    sing_it = recomC.recommend(params[:id])
+    render json: sing_it
   end
   
   
