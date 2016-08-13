7374498edf793be0afce6d784df69173efc4553d
yhk1038 <yhk1038@hanmail.net>
2016-08-13 16:15:50 +0900

    크롤러 0.8 (#14)
    * 추천 전용 컨트롤러
    
    * 추천 컨트롤러 1차 정리 끝
    
    * 오타수정1
    
    * 머지 수정
    
    * 크롤러0.8

---
+++ config/schedule.rb
@@ -0,0 +1,78 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
+
+set :environment, "development"
+
+every 10.minute do
+  # exec
+  runner "CrawlController.run", :environment => "development"
+  
+  # dump ready
+  command "mkdir /home/ubuntu/workspace/public/seeds/#{Time.new.to_s.gsub('-','')}"
+  command "cp /home/ubuntu/workspace/db/seeds.rb /home/ubuntu/workspace/public/seeds/#{Time.new.to_s.gsub('-','')}/seed.rb"
+  
+  # dump
+  rake "db:seed:dump"
+end
+
+
+
+
+
+
+# 1.minute 1.day 1.week 1.month 1.year is also supported
+# every 3.hours do 
+#   runner "MyModel.some_process"
+#   rake "my:rake:task"
+#   command "/usr/bin/my_great_command"
+# end
+
+# every 1.day, :at => '4:30 am' do
+#   runner "MyModel.task_to_run_at_four_thirty_in_the_morning"
+# end
+
+
+# Many shortcuts available: :hour, :day, :month, :year, :reboot
+# every :hour do 
+#   runner "SomeModel.ladeeda"
+# end
+
+
+# Use any day of the week or :weekend, :weekday
+# every :sunday, :at => '12pm' do 
+#   runner "Task.do_something_great"
+# end
+
+# every '0 0 27-31 * *' do
+#   command "echo 'you can use raw cron syntax too'"
+# end
+
+# # run this task only on servers with the :app role in Capistrano
+# # see Capistrano roles section below
+# every :day, :at => '12:20am', :roles => [:app] do
+#   rake "app_server:task"
+# end
+
+
+# job_type :command, ":task :output"
+# job_type :rake,    "cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
+# job_type :runner,  "cd :path && bin/rails runner -e :environment ':task' :output"
+# job_type :script,  "cd :path && :environment_variable=:environment bundle exec script/:task :output"
