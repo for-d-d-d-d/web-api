8218da930d604991d8a51fcd625d9c894bc58b73
likelionsunwoo <sunwoolyu@naver.com>
2016-11-11 01:54:37 +0900

    페이스북로그인 작업함

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,6 +39,22 @@ class User < ActiveRecord::Base
         end
     end
 
+
+    def self.find_for_facebook_oauth(auth)
+        user = where(auth.slice(:provider, :uid)).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+        user.name = auth.info.name   # assuming the user model has a name
+        user.image = auth.info.image # assuming the user model has an image
+         # 이 때는 이상하게도 after_create 콜백이 호출되지 않아서 아래와 같은 조치를 했다.
+        user.add_role :user if user.roles.empty?
+        user   # 최종 반환값은 user 객체이어야 한다.
+        end
+    end   
+
+    
     def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
         data = access_token.info
         user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
